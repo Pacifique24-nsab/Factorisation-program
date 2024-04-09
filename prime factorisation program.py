dbd27f44db69f6c1cabc06d487e7154327d3a409
Pacifique Nsabumukiza <133487528+Pacifique24-nsab@users.noreply.github.com>
2024-04-09 21:35:22 +0300

    Update prime factorisation program..(comment)

--- prime factorisation program.py
+++ prime factorisation program.py
@@ -1,7 +1,7 @@
 #welcome to the program of generating all prime number between 2 and n
 import time( to know how much time was spent to process the program)
 
-# welcome message from the user
+# welcome message from the user ( as a starting message)
 print("Welcome to the prime factorization program  made by pacifique!!!")
 
 # define a function that takes a number and returns its prime factors
